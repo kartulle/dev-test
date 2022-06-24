abd23eaf7d2be06c27e22aeb3f1c523c43da5f14
Karolaine <kartulle@hotmail.com>
2022-06-24 00:19:07 -0300

    feat: Formatando a Data

---
+++ app.js
@@ -0,0 +1,24 @@
+const data = document.querySelector("#date");
+const btnFormatarData = document.querySelector("#btnFormatarData");
+
+function formatDate() 
+{
+    const dataValor = data.value;
+    const novaData = dataValor.split("-")
+    const dia = novaData[2];
+    const mes = novaData[1];
+    const ano = novaData[0];
+    console.log(`${ano}${mes}${dia}`);
+
+     const htmlDataFormatada = `A nova data formatada é ${ano}${mes}${dia}`;
+    
+    const dataFormatada = document.querySelector('.dataFormatada')
+
+    dataFormatada.innerHTML = htmlDataFormatada
+
+
+}
+
+btnFormatarData.onclick = () => {
+    formatDate();
+}
