019e30d73d68806f00e4990addcc259b65a7ee20
Karolaine <kartulle@hotmail.com>
2022-06-24 21:41:55 -0300

    finalizado 2.0

--- testes/js/imageGalery/baseConfig.js
+++ testes/js/imageGalery/baseConfig.js
@@ -4,12 +4,12 @@ const div1 = document.getElementsByClassName('image')
 remove[0].onclick = function(){
     if(!(div1[0].classList.contains("hide"))){
         div1[0].classList.add("hide")
-        div1[1].classList.remove("hide")
+      
     }
 }
 remove[1].onclick = function(){
     if(!(div1[1].classList.contains("hide"))){
         div1[1].classList.add("hide")
-        div1[0].classList.remove("hide")
+       
     }
 }
